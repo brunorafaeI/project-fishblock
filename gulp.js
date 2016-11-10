d9154cdccc63b36579cb5109f7d0f979f26b6e3f
Bruno Rafael <brunorafaelap@gmail.com>
2016-11-10 22:25:42 +0100

    dependencies installed bootstrap-sass, fa-awesome and plugin.sass

---
+++ gulp.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//task for the sass
+gulp.task('sass', function(){
+   gulp.src('web/public/sass/**/*.sass')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('web/public/css'))
+})
+
+//task for watch
+gulp.task('watch', () =>
+    gulp.watch('web/public/sass/**/*.sass', ['sass'])
+)
+
+//task for default gulp
+gulp.task('default', ['sass', 'watch'])
