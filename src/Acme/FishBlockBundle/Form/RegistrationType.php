f4b6b6987349428486c8f77656f38d849b6c2e0b
Bruno Rafael <brunorafaelap@gmail.com>
2016-11-25 11:43:35 +0100

    installation tinymce and update files userbundle

---
+++ src/Acme/FishBlockBundle/Form/RegistrationType.php
@@ -0,0 +1,40 @@
+<?php
+// src/Acme/FishBlockBundle/Form/RegistrationType.php
+
+namespace Acme\FishBlockBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Gregwar\CaptchaBundle\Type\CaptchaType;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+
+            ->add('date_nais', BirthdayType::class, array('format' => 'ddMMyyyy', 'label' => 'Date de naissance: '))
+            ->add('captcha', CaptchaType::class, array('reload' => true, 'as_url' => true))
+        ;
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+        // Or for Symfony < 2.8
+        // return 'fos_user_registration';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    // For Symfony 2.x
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
