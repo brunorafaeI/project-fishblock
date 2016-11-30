71f10e670a1923f91eef45c6090be86435e6d087
Kevin H <kevin.herniou@gmail.com>
2016-11-30 13:01:35 +0100

    datafixtures completed

--- src/Acme/FishBlockBundle/DataFixtures/ORM/LoadingFixtures.php
+++ src/Acme/FishBlockBundle/DataFixtures/ORM/LoadingFixtures.php
@@ -119,7 +119,8 @@ class LoadingFixtures implements FixtureInterface
         // On crée la série !
         $Serie = new Series();
         $Serie->setTitre("Game of thrones");
-        $Serie->setDescription("Game of thrones, également désignée par le titre français de l'oeuvre dont elle est adaptée, Le trône de fer, est une série télévisée américaine médiéval(fantastique créée par david benioff et D. B. weiss,diffusée depuis le 17 avril 2011 sur HBO");
+        $Serie->setImage("got.jpg");
+        $Serie->setDescription("Sur le continent de Westeros, le roi Robert Baratheon gouverne le Royaume des Sept Couronnes depuis plus de dix-sept ans, à la suite de la rébellion qu'il a menée contre le « roi fou » Aerys II Targaryen. Jon Arryn, époux de la sœur de Lady Catelyn Stark, Lady Arryn, son guide et principal conseiller, vient de décéder, et le roi part alors dans le nord du royaume demander à son vieil ami Eddard « Ned » Stark de remplacer leur regretté mentor au poste de Main du roi. Ned, seigneur suzerain du nord depuis Winterfell et de la maison Stark, est peu désireux de quitter ses terres. Mais il accepte à contre-cœur de partir pour la capitale Port-Réal avec ses deux filles, Sansa et Arya. Juste avant leur départ pour le sud, Bran, l'un des jeunes fils d'Eddard, est poussé de l'une des tours de Winterfell après avoir été témoin de la liaison incestueuse de la reine Cersei Baratheon et son frère jumeau, Jaime Lannister. Leur frère, Tyrion Lannister, surnommé « le gnome », est alors accusé du crime par Lady Catelyn Stark.");
         $Serie->getListeDesCategories()->add($Dramatique);
         $Serie->getListeDesCategories()->add($Fantasy);
         // Enregistrment dans la base de données
@@ -129,7 +130,10 @@ class LoadingFixtures implements FixtureInterface
         // On crée la série !
         $Serie = new Series();
         $Serie->setTitre("Supernatural");
-        $Serie->setDescription("Supernatural est une série télévisée américaine créée par Eric Kripke et produite par McG un producteurs exéctutifs.");
+        $Serie->setImage("sn.jpg");
+        $Serie->setDescription("Deux frères, Sam et Dean Winchester, chasseurs de créatures surnaturelles, sillonnent les États-Unis à bord d'une Chevrolet Impala noire de 1967 et enquêtent sur des phénomènes paranormaux (souvent issus du folklore, des superstitions, mythes et légendes urbaines américaines, mais aussi des monstres surnaturels tels que les fantômes, loups-garous, démons, vampires…).
+
+De manière générale, chaque épisode se déroule dans un lieu différent du pays et correspond à une enquête sur un phénomène paranormal.");
         $Serie->getListeDesCategories()->add($Dramatique);
         $Serie->getListeDesCategories()->add($Fantasy);
         $Serie->getListeDesCategories()->add($Horreur);
@@ -140,7 +144,9 @@ class LoadingFixtures implements FixtureInterface
         // On crée la série !
         $Serie = new Series();
         $Serie->setTitre("The walking dead");
-        $Serie->setDescription("The walking dead est une série télévisée d'horreur / dramatique américaine, adaptée par Frank Darabont et Robet Kirkman, créateur de la bande dessiné du même nom, diffusée depuis le 31 octobre 2010 sur AMC");
+        $Serie->setImage("twd.jpg");
+        $Serie->setDescription("Après une épidémie post-apocalyptique ayant transformé la quasi-totalité de la population américaine et mondiale en mort-vivants ou « rôdeurs », un groupe d'hommes et de femmes mené par l'adjoint du shérif du comté de Kings (en Géorgie), Rick Grimes, qui se réveille tout juste d'un coma, tente de survivre…
+Ensemble, ils vont devoir tant bien que mal faire face à ce nouveau monde devenu méconnaissable, à travers leur périple dans le Sud profond des États-Unis.");
         $Serie->getListeDesCategories()->add($Dramatique);
         $Serie->getListeDesCategories()->add($Horreur);
         // Enregistrment dans la base de données
@@ -150,6 +156,7 @@ class LoadingFixtures implements FixtureInterface
         // On crée la série !
         $Serie = new Series();
         $Serie->setTitre("Dexter");
+        $Serie->setImage("dexter.jpg");
         $Serie->setDescription("Expert en médecine légale spécialisé dans l'analyse de traces de sang dans la police le jour, tueur en série la nuit, Dexter Morgan n'est pas exactement un citoyen américain comme les autres. Il porte, en effet, un lourd secret. Traumatisé dans sa plus tendre enfance par le meurtre de sa mère, il a été ensuite recueilli par un officier de police de Miami. Il se dit incapable de ressentir la moindre émotion. Incapable, si ce n'est lorsqu'il satisfait les pulsions meurtrières que son père adoptif lui a appris à canaliser : de fait, Dexter ne tue que des criminels qui échappent au système judiciaire. Bien que sa soif de tuer lui pèse, il parvient à mener une existence relativement normale et à sauver les apparences auprès de ses collègues, amis et petite amie.");
         $Serie->getListeDesCategories()->add($Dramatique);
         $Serie->getListeDesCategories()->add($Horreur);
@@ -160,6 +167,7 @@ class LoadingFixtures implements FixtureInterface
         // On crée la série !
         $Serie = new Series();
         $Serie->setTitre("Hero corp");
+        $Serie->setImage("hc.jpg");
         $Serie->setDescription("À la suite d’une guerre dans les années 1980, il est décidé de créer l’agence Hero Corp, une organisation regroupant tous les super-héros afin de maintenir un climat de paix. Cette agence possède plusieurs sites secrets sur la planète et dans le département de la Lozère se trouvent les retraités, mis au rancart, démissionnaires, démasqués, pas-formés. Coupés du monde, ils peuvent retrouver une vie calme et paisible. Vingt ans après, ce calme paisible vole en éclats lorsque réapparaît The Lord.
 				Face au retour de The Lord, le plus grand super-vilain de l’Histoire, le village est démuni. Selon une prédiction, John est l'unique solution face à ce danger que Hero Corp préfère garder sous silence.
 John arrive dans un village isolé pour aller enterrer sa tante. Il se rend compte que les habitants cachent quelque chose et qu’ils n’ont pas l’air décidés à le laisser partir.");
@@ -171,6 +179,7 @@ John arrive dans un village isolé pour aller enterrer sa tante. Il se rend comp
         // On crée la série !
         $Serie = new Series();
         $Serie->setTitre("Vikings");
+        $Serie->setImage("vikings.jpg");
         $Serie->setDescription("La série suit les exploits d'un groupe de Vikings mené par Ragnar Lothbrok, l'un des plus populaires héros vikings de tous les temps au destin semi-légendaire. Ragnar serait d'origine danoise, suédoise ou encore norvégienne selon les sources. Il est supposé avoir unifié les clans vikings en un royaume aux frontières indéterminées à la fin du VIIIe siècle (le roi Echbert mentionne avoir vécu à la cour du roi Charlemagne, sacré empereur en l'an 800). Mais il est surtout connu pour avoir été le promoteur des tous premiers raids vikings en terres chrétiennes, saxonnes, franques ou celtiques.
 Ce simple fermier, homme lige du jarl Haraldson, se rebelle contre les choix stratégiques de son suzerain. Au lieu d'attaquer les païens slaves et baltes de la Baltique, il décide de se lancer dans l'attaque des riches terres de l'Ouest, là où les monastères regorgent de trésors qui n'attendent que d'être pillés par des guerriers ambitieux.
 Clandestinement, Ragnar va monter sa propre expédition et sa réussite changera le destin des Vikings comme celui des royaumes chrétiens du sud, que le simple nom de « Vikings » terrorisera pendant plus de deux siècles.");
